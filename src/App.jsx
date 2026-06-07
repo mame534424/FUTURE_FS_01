9c20025a67a461acaa1c8e4be688eec83e79555d
Mohammed Abrar <mame.534424@gmail.com>
2026-06-07 11:47:56 +0300

    Add Home and NotFound components; update imports in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./Pages/Home"
-import NotFound from "./Pages/NotFound"
+import Home from "./components/Pages/Home"
+import NotFound from "./components/Pages/NotFound"
 
 
 function App() {
